18edaacc26662205b31901e20e707ba83f18ed95
nileshgarg0023 <nileshgarg0023@gmail.com>
2024-02-20 14:34:09 +0530

    basic routing and some util file with minor bug resolved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 
 connectDB()
@@ -11,10 +12,10 @@ connectDB()
     app.listen(process.env.PORT || 8000, () => {
       console.log(` Server is running at port: ${process.env.PORT}`);
     });
-    app.on("error", (error) => {
-      console.log("error while starting server", error);
-      throw error;
-    });
+    // app.on("error", (error) => {
+    //   console.log("error while starting server", error);
+    //   throw error;
+    // });
   })
   .catch((err) => {
     console.log("MONGO db connection failed !!!", err);
